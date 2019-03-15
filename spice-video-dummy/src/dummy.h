0980d14bd07d6cd66824482d23f6cc2fca2438ec
Jeremy White <jwhite@codeweavers.com>
2019-03-15 16:12:16 -0500

    Add the upstream xf86-video-dummy as of November 25, 2018.
    This gives x11spice access to a modern virtual/offscreen
    frame buffer driver for the Xorg X server.  We will then
    make slight revisions to make it more suitable for use with Spice.
    
    The overall result should be a more functional stack,
    as compared to the existing xf86-video-qxl driver, as we gain
    a wide range of Mesa features for free.  It also simplifies
    the overall process.
    
    This is from https://gitlab.freedesktop.org/xorg/driver/xf86-video-dummy
    as of 850c0516.

---
+++ spice-video-dummy/src/dummy.h
@@ -0,0 +1,59 @@
+
+/* All drivers should typically include these */
+#include "xf86.h"
+#include "xf86_OSproc.h"
+
+#include "xf86Cursor.h"
+
+#ifdef XvExtension
+#include "xf86xv.h"
+#include <X11/extensions/Xv.h>
+#endif
+#include <string.h>
+
+#include "compat-api.h"
+
+/* Supported chipsets */
+typedef enum {
+    DUMMY_CHIP
+} DUMMYType;
+
+/* function prototypes */
+
+extern Bool DUMMYSwitchMode(SWITCH_MODE_ARGS_DECL);
+extern void DUMMYAdjustFrame(ADJUST_FRAME_ARGS_DECL);
+
+/* in dummy_cursor.c */
+extern Bool DUMMYCursorInit(ScreenPtr pScrn);
+extern void DUMMYShowCursor(ScrnInfoPtr pScrn);
+extern void DUMMYHideCursor(ScrnInfoPtr pScrn);
+
+/* globals */
+typedef struct _color
+{
+    int red;
+    int green;
+    int blue;
+} dummy_colors;
+
+typedef struct dummyRec 
+{
+    /* options */
+    OptionInfoPtr Options;
+    Bool swCursor;
+    /* proc pointer */
+    CloseScreenProcPtr CloseScreen;
+    xf86CursorInfoPtr CursorInfo;
+
+    Bool DummyHWCursorShown;
+    int cursorX, cursorY;
+    int cursorFG, cursorBG;
+
+    dummy_colors colors[1024];
+    Bool        (*CreateWindow)() ;     /* wrapped CreateWindow */
+    Bool prop;
+} DUMMYRec, *DUMMYPtr;
+
+/* The privates of the DUMMY driver */
+#define DUMMYPTR(p)	((DUMMYPtr)((p)->driverPrivate))
+
